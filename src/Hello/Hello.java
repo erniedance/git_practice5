4c5c721c410857882707c250184445a2b3c319ba
ernie <edance1775@gmail.com>
2022-01-24 11:11:23 -0500

    created main

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -6,6 +6,11 @@ public class Hello {
     // commit 1
     // commit 2
     // commit 3
+    public static void main(String[] args) {
+
+        System.out.println("Hello World ");
+        System.out.println("B25 learning GitHub");
+    }
 
 
 
